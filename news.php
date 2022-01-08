72101a96e5c4cd1877f69596497782fa8fc23463
rwema3 <rwemadominik3@gmail.com>
2022-01-08 19:12:24 +0200

    php & mysql
    php & mysql

---
+++ news.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Top Club News</title>
+	
+<link rel="icon" type="image/png" href="images/uclogo.jpg">
+<link rel="stylesheet" type="text/css" href="css/indexs.css">
+<link rel="stylesheet" type="text/css" href="css/news.css">
+
+<style type="text/css">
+   .rwema{
+
+    background: white;
+    background: -webkit-linear-gradient(top, blue 0%,#00CCCC  100%);
+    background: -moz-linear-gradient(top, blue 0%,#00CCCC  100%);
+    background: -o-linear-gradient(top, blue 0%,#00CCCC  100%);
+    background: -ms-linear-gradient(top, blue 0%,#00CCCC  100%);
+    background: linear-gradient(top, blue 0%,#00CCCC  100%);
+    -webkit-border-radius: none;
+    -moz-border-radius: none;
+    border-radius: none;
+    z-index:2;
+    border: 2px;
+    margin-left: 215px;
+    color: white;
+    text-align: center;
+    font-family: cursive;
+    font-size: 16px;
+    width: 900px;
+    height: 40px;
+    margin-left: 220px;
+}
+</style>
+</head>
+
+<body background="images/back.JPG">
+
+<table border="0" align="center"  width="680">
+
+<tr><td colspan="3"> <center><img src="images/ramos.jpg" width="900" height="140"></center> </td></tr>
+
+<tr><td align="center" colspan="3">
+
+<ul class="menu">
+    <li><a href="index.php">Who we are</a></li>
+    <li><a href="membership.php">registration & login</a></li>
+  <li><a href="direction.php">Administration</a></li>
+    <li><a href="news.php">Unity Club News</a></li>
+   <li><a href="susponser.php">Sponsors form</a></li>
+    <li><a href="contactus.php">Contact Us</a></li>
+</ul>
+</td></tr>
+</table>
+
+<table border="0" align="center" height="110" width="900" style="margin-top: -50px;">
+<tr>
+<td>
+
+<div class="unitynews">
+<div class="uclub">
+<img src="images/DSC_0096.jpg">						
+</div>
+
+<div class="unity">
+<span>
+<font color="black" size="2"> <b si>Unity Club joins reconciliation drive
+<a href="http://www.newtimes.co.rw/section/read/213065/" style="font-family: cursive; font-size: 12px; color:white;">read</a></b></font>
+</span>	
+</div>            
+</div>
+
+</td>
+
+<td >
+
+<div class="unitynews">
+<div class="uclub">
+<img src="images/uclogo.jpg">						
+</div>
+
+<div class="unity">
+<span>
+<font color="black" size="2"><b>Karongi:New Branch of Unity Club</b>	
+<a href="http://www.newtimes.co.rw/section/read/213065/" style="font-family: cursive; font-size: 12px; color:white;">continueread</a></b></font>
+</font>	
+</span>						                                                           	
+</div>            
+</div>
+</td><td >
+
+<div class="unitynews">
+<div class="uclub">
+<img src="images/UNTY (1).jpg">						
+</div>
+
+<div class="unity">
+<font color="black" size="2"><b>Clebrating 20<sup>th</sup> years old of Club   
+<a href="http://www.newtimes.co.rw/section/read/213065/" style="font-family: cursive; font-size: 12px; color:white;">continueread</a></b></font>
+</b>	</font>	
+</span>						                                                           	
+</div>            
+</div>
+
+</td><td >
+
+<div class="unitynews">
+<div class="uclub">
+<img src="images/read.jpg">						
+</div>
+
+<div class="unity">
+<span>
+<font color="black" size="2"><b>10 members registred last 10 hours 
+<a href="images/list.jpg" style="font-family: cursive; font-size: 12px; color:white;">continueread</a></b></font>
+</b>
+</font>	
+</span>						                                                           	
+</div>            
+</div>
+
+</td>
+</td>
+</tr>
+</table>
+
+<table border="0" align="center" height="110" width="900">
+<tr>
+<td>
+
+<div class="unitynews">
+<div class="uclub">
+<img src="images/student 90.jpg">						
+</div>
+
+<div class="unity">
+<span>
+<font color="black" size="2"> <b>remain 2the national exams to begin </b></font>
+<a href="http://www.unityclub.gov.rw/section/read/213065/images" style="font-family: cursive; font-size: 12px; color:white;">continueread</a></b></font>
+</span>	
+</div>            
+</div>
+
+</td>
+
+<td >
+
+<div class="unitynews">
+<div class="uclub">
+<img src="images/dami.jpg">						
+</div>
+
+<div class="unity">
+<span>
+<font color="black" size="2"><b>Design Is our Talent</b>	</font>	
+<a href="images/list.jpg" style="font-family: cursive; font-size: 12px; color:white;">continueread</a></b></font>
+</span>						                                                           	
+</div>            
+</div>
+
+</td><td>
+
+<div class="unitynews">
+<div class="uclub">
+<img src="images/post.jpg">						
+</div>
+
+<div class="unity">
+<span>
+<font color="black" size="2"><b>Sport:CLub members Vs Readers 5:00pm tomorrow 
+<a href="http://www.newtimes.co.rw/section/read/213065/" style="font-family: cursive; font-size: 12px; color:white;">matchprediction</a></b></font>
+
+</b>	</font>	
+</span>						                                                           	
+</div>            
+</div>
+
+</td><td >
+
+<div class="unitynews">
+<div class="uclub">
+<img src="images/police.png">						
+</div>
+
+<div class="unity">
+<span>
+<font color="black" size="2"><b>new relatinship betten unity and police station
+<a href="http://www.newtimes.co.rw/section/read/213065/" style="font-family: cursive; font-size: 12px; color:white;">continueread</a></b></font>
+</b>	</font>	
+</span>						                                                           	
+</div>            
+</div>
+
+</td>
+</td>
+</tr>
+
+</table>
+
+<table border="0" align="center" height="40" width="900">
+<tr>
+<td bgcolor="white" align="center"><font size="4" color="#000000">Continue Reading  On This pages: <a href="#">|1|</a><a href="#.php">|2|</a><a href="#.php">|3|</a><a href="#.php">|4|</a><a href="#.php">|5|</a></font>
+</td></tr>
+</table>
+</td></tr>
+</table>  
+
+<button class="rwema"><font size="2" color="white">&copy by | Bagirishya Rwema Dominique | 2017.</button>
+</body>
+</html>
